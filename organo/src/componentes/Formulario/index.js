36d4ef32e9cec03faa3280cfea5c57d071beecfc
CleanoJr <cleanojr@gmail.com>
2023-08-18 23:41:19 -0300

    Inserir Botão 'Criar Card'

--- organo/src/componentes/Formulario/index.js
+++ organo/src/componentes/Formulario/index.js
@@ -1,18 +1,18 @@
-import './Formulario.css'
-import CampoTexto from '../CampoTexto'
-import ListaSuspensa from '../ListaSuspensa';
+import "./Formulario.css";
+import CampoTexto from "../CampoTexto";
+import ListaSuspensa from "../ListaSuspensa";
+import Botao from "../Botao";
 
 const Formulario = () => {
-
-    const times = [
-        'Programação',
-        'Front-End',
-        'Data Science',
-        'DevOps',
-        'UX e Design',
-        'Mobile',
-        'Inovação e Gestão'
-    ]
+  const times = [
+    "Programação",
+    "Front-End",
+    "Data Science",
+    "DevOps",
+    "UX e Design",
+    "Mobile",
+    "Inovação e Gestão",
+  ];
 
   return (
     <section className="formulario">
@@ -22,6 +22,7 @@ const Formulario = () => {
         <CampoTexto label="Cargo" placeholder="Digite seu cargo..." />
         <CampoTexto label="Imagem" placeholder="Digite o endereço da imagem..." />
         <ListaSuspensa label="Time" itens={times} />
+        <Botao>Criar Card</Botao>
       </form>
     </section>
   );
